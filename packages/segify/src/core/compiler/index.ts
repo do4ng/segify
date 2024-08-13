2d446b544e37b54476636f9cf61b9b2f66119422
r.ehdals <57896501+do4ng@users.noreply.github.com>
2024-08-13 23:58:56 +0900

    feat: export `{ Component }`

--- packages/segify/src/core/compiler/index.ts
+++ packages/segify/src/core/compiler/index.ts
@@ -194,7 +194,7 @@ export async function compile(
 }`);
 
   if (options?.noExport !== true) {
-    file.push('export default Component;');
+    file.push('export { Component, Component as default}');
   }
 
   return file.join('\n');
